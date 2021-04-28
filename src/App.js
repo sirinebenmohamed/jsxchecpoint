284bb034fb16decd15d580c09b5869737359450d
sirine ben mohamed <sirinebenmohamed777@gmail.com>
2021-04-28 15:06:21 +0200

    my jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import myImageInSrc from "./imageInSrc.jpg";
 import './App.css';
+import video from "./myVideo.mp4";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">Syrine Ben Mohamed</h1>
+          <br />
+          <h3>Hello I'm a Full Stack JS Student</h3>
+          <br/>
+          <img src={myImageInSrc} width={560} height={315}/> 
+          <img src="/imageInPublic.jpg" />
+        </div>
+        <div>
+          <h3>What Is React (React js) and Why Is It So Popular?</h3>
+        <video width={700} height={440} controls>
+          <source src={video} type="video/mp4" />
+        </video>
+        </div>
+        
+      </div>
   );
 }
 
